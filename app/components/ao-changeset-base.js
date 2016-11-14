6aca34a44509a24ef26201cd6d4e82fc67f6e7dc
Gabe Markarian <gabemarkarian@Gabes-MBP.fios-router.home>
2016-11-14 08:44:19 -0500

    importing mixin from an extending component

---
+++ app/components/ao-changeset-base.js
@@ -0,0 +1,2 @@
+import AoChangesetBase from 'ao-changeset-base/mixins/changeset-base';
+export default Ember.Component.extend(AoChangesetBase);
--- app/mixins/ao-changeset-base.js
+++
@@ -1,4 +0,0 @@
-import Ember from 'ember';
-import AoChangesetBase from 'ao-changeset-base/mixins/ao-changeset-base';
-
-export default AoChangesetBase;
