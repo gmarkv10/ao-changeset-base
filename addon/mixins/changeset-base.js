2da04639d00875024fe5ea6fd6321c17cc18f103
Gabe Markarian <gabemarkarian@Gabes-MBP.fios-router.home>
2016-11-15 08:57:33 -0500

    added beforeValidate, beforeSave and handleErrors hooks that are implemented as computed properties

--- addon/mixins/changeset-base.js
+++ addon/mixins/changeset-base.js
@@ -1,76 +1,85 @@
 import Ember from 'ember';
 const ZERO = 0;
 export default Ember.Mixin.create({
-	init(){
-		this._super(...arguments);
-		this.set('context', this);
-	},
 	actions: {
+		/**
+		 * There are 4 outcomes when a beforeSave/Validate function is given in the extending component
+		 * They depenend on what is returned by beforeSave/Validate)
+		 * 
+		 * 1. beforeSave/Validate = false : Stop propagation of any actions
+		 * 2. beforeSave/Validate = {force: false} : Fall through to bubbling the save action, no force
+		 * 3. beforeSave/Validate = {force: true} : Fall through to bubbling the save action, with force
+		 * 4. beforeSave/Validate = undefined : Run the rest of the save action (as if no beforeSave was called)
+		 */
 		onSave(force){
-			const beforeSave = this.get('beforeSave');
+			this.set('onSaveFired', !(this.get('onSaveFired'))); //can be used to listen for all onSave events
+			let changeset = this.get(`${this.get('changeset')}`);
 			let forceClose;
-			/**
-			 * There are 4 outcomes when a beforeSave function is given in the extending component
-			 * They depenend on what is returned by beforeSave (the beforeSaveResult)
-			 * 
-			 * 1. beforeSaveResult = false : Stop propagation of any actions
-			 * 2. beforeSaveResult = {force: false} : Fall through to bubbling the save action, no force
-			 * 3. beforeSaveResult = {force: true} : Fall through to bubbling the save action, with force
-			 * 4. beforeSaveResult = undefined : Run the rest of the save action (as if no beforeSave was called)
-			 */
-			if(beforeSave !== undefined){
-				if(beforeSave === false){
-					return false;
+
+			const beforeValidate = this.get('beforeValidate');
+			if(beforeValidate !== undefined){
+				if(beforeValidate === false){
+					return false; //stop propagation
 				}
-				if(typeof beforeSave === 'object'){
-					forceClose = beforeSave.force;
+				if(typeof beforeValidate === 'object'){
+					forceClose = beforeValidate.force;
 				}
 			}
 
-			if(forceClose === undefined){
-				let changeset = this.get(`${this.get('changeset')}`);
+			if(!forceClose && !force){
+				
 				changeset.validate();
-				if(changeset.get('errors.length')){
+				if(!changeset.get('errors.length')){		
+					const beforeSave = this.get('beforeSave');
+			
+					if(beforeSave !== undefined){
+						if(beforeSave === false){
+							return false; //stop propagation
+						}
+						if(typeof beforeSave === 'object'){
+							forceClose = beforeSave.force;
+						}
+					}
+
+					changeset.validate(); //must vaildate possible changes in beforeSave
+				}
+				if(changeset.get('errors.length')){ //not an else b/c its a new test after validate call above
 					let changesetErrors = changeset.get('errors').map((err) => {
 						return err.validation[ZERO]; 
-					}); 
-					if(this.get('displayErrors') && this.get('displayErrors').display){
-						this.get('displayErrors').display(changesetErrors);
-					}
-					return false;
+					});
+					this.set('changesetErrors', changesetErrors); 
+					this.get('handleOnSaveErrors');
+					return false; //stop propagation
+					//changeset.rollback(); ?
 				}
-				changeset.execute();
 			}
+			changeset.execute(); //this is safe, won't result in any changes if there are errors
 			Ember.run.schedule('actions', this, function() {
-				this.sendAction('onSave', forceClose || force); //eslint-disable-line no-invalid-this
+				this.sendAction('onSave', forceClose || force); 
 			});	
-			return false;
+			return false; //stop propagation, use explicit send action above
 		},
 		onClose(force){
+			this.set('onCloseFired', !(this.get('onCloseFired'))); //can be used to listen for all onClose events
+			
 			let changeset = this.get(`${this.get('changeset')}`);
-			if(this.get('checkRouteForDirty')){
-				this.set('needsConfirm', this.get('checkRouteForDirty')());
-			} else {
-				this.set('needsConfirm', changeset.get('changes.length') > ZERO);
-			}
-			if (this.get('isDirty')){
-				this.set('needsConfirm', true);	
-			}			
+				
 			changeset.validate();
 			if(changeset.get('errors.length')){
 				this.set('isInvalid', true);
 				let changesetErrors = changeset.get('errors').map((err) => {
 					return err.validation[ZERO]; 
 				}); 
-				this.set('errorList', changesetErrors);
+				this.set('changesetErrors', changesetErrors);
+				this.get('handleOnCloseErrors');
 			} else {
 				changeset.execute();
-				this.set('needsConfirm', changeset.get('changes.length') > ZERO);
+				//changeset.rollback(); ?
 			}
-			if (force || (!this.get('needsConfirm') && !this.get('isInvalid')) ){
+			if (force || (!this.get('isDirty') && !this.get('isInvalid')) ){
 				Ember.$('.ui.modal').modal('hide');
 				Ember.run.schedule('actions', this, function() {
-					this.sendAction('onCancel'); //eslint-disable-line no-invalid-this
+					this.sendAction('onCancel'); 
 				});	
 			}
 			return false;
